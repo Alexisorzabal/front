2ea70c8b8f19ffc5ad8e2d7bbf57304b62d400a8
Alexisorzabal <106978463+Alexisorzabal@users.noreply.github.com>
2023-09-07 08:06:18 -0300

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from './components/Nav';
+import Footer from './components/Footer/Footer';
+import Contact from './pages/Contact';
+import HomePage from './pages/HomePage';
+import Projects from './pages/Projects';
+import Experience from './pages/Experience'
 import './App.css';
+const App = () => {
 
-function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <BrowserRouter>
+        <Nav/>
+        <Routes>
+          <Route path="/"element={<HomePage/>} />
+          <Route path="Experience"element={<Experience/>} />
+          <Route path="Projects"element={<Projects/>} />
+          <Route path="Contact"element={<Contact/>} />
+        </Routes>
+      </BrowserRouter>
+      
+      <Footer/>
+    
     </div>
   );
 }
-
 export default App;
---
+++ src/pages/Projects.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/projects.css';
+const Projects = (props) => {
+    return (
+       <div>
+        Projects
+       </div>
+    );
+}
+export default Projects;
